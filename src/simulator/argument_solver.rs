2bba3b1a029465a1b0058ceabc8b249c5a8463fc
Salvador de la Puente González <salva@unoyunodiez.com>
2020-01-18 12:46:14 +0100

    Add test for argument solver

--- src/simulator/argument_solver.rs
+++ src/simulator/argument_solver.rs
@@ -20,7 +20,16 @@ impl<'a> ArgumentSolver<'a> {
 #[cfg(test)]
 mod test {
   use super::*;
-  use grammar::ast::*;
-  use std::f64::consts::PI;
+  use std::iter::FromIterator;
 
+  #[test]
+  fn test_replace_formal_parameter_with_actual_parameter() {
+    let actual_argument = ast::Argument::Item("actual".to_owned(), 0);
+    let bindings = HashMap::from_iter(vec![
+      ("formal".to_owned(), actual_argument.clone())
+    ]);
+    let solver = ArgumentSolver::new(&bindings);
+    let formal_argument = ast::Argument::Id("formal".to_owned());
+    assert_eq!(solver.solve(&formal_argument), actual_argument);
+  }
 }
