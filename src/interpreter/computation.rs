7b882f61fb89fce85d8ccf1fcb96af2a220858f9
Salvador de la Puente González <salva@unoyunodiez.com>
2020-02-21 14:33:09 +0100

    Match WASM interface

--- src/interpreter/computation.rs
+++ src/interpreter/computation.rs
@@ -6,13 +6,21 @@ use crate::statevector::StateVector;
 
 #[derive(Debug)]
 pub struct Computation {
-  pub statevector: StateVector,
-  pub memory: HashMap<String, u64>
+  statevector: StateVector,
+  memory: HashMap<String, u64>
 }
 
 impl Computation {
   pub fn new(memory: HashMap<String, u64>, statevector: StateVector) -> Self {
     Computation{ statevector, memory }
+  }
+
+  pub fn statevector(&self) -> &StateVector {
+    &self.statevector
+  }
+
+  pub fn memory(&self) -> HashMap<String, u64> {
+    self.memory.clone()
   }
 
   pub fn probabilities(&self) -> Vec<f64> {
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 extern crate qasmsim;
 
 use std::process;
-use std::io::{self, Read};
+use std::io::{ self, Read };
 use std::time::Instant;
 
 fn main() -> io::Result<()> {
   let start = Instant::now();
   let mut input = String::new();
+
   io::stdin().read_to_string(&mut input)?;
-  let state_vector = qasmsim::run(&input).unwrap_or_else(|err| {
+  let result = qasmsim::run(&input);
+
+  if let Err(err) = result {
     println!("An error happened: {}", err);
     process::exit(1);
-  }).statevector;
+  }
+
+  let computation = result.unwrap();
+  let state_vector = computation.statevector();
+
   println!("{:?}", state_vector);
   println!("Calculated {} item statevector in {:?}s", state_vector.len(), start.elapsed().as_secs_f32());
   Ok(())
