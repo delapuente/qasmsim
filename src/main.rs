c4e0dee73d098ed29c70a132f2471a452be7f2cb
Salvador de la Puente González <salva@unoyunodiez.com>
2020-03-29 18:06:13 +0200

    Improve CLI output

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,13 @@ use std::io::{ self, Read };
 use std::fs;
 use std::path::PathBuf;
 use std::fmt::{ self, Write };
+use std::iter::FromIterator;
+
+use structopt::StructOpt;
+use prettytable::{ Table, row, cell, format };
 
 use qasmsim::{ Run, RunTimes, Histogram };
 use qasmsim::statevector::StateVector;
-use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "qasmsim", about = "A QASM interpreter and quantum simulator in Rust.")]
@@ -21,6 +24,22 @@ struct Options {
   /// Output file, stdout if not present.
   #[structopt(long)]
   output: Option<PathBuf>,
+
+  /// Verbosity of the output.
+  #[structopt(short, parse(from_occurrences))]
+  verbose: u64,
+
+  /// Prints the binary representation of the values.
+  #[structopt(long, short="b")]
+  binary: bool,
+
+  /// Prints the hexadecimal representation of the values.
+  #[structopt(long, short="x")]
+  hexadecimal: bool,
+
+  /// Prints the interger representation of the values. Default option.
+  #[structopt(long, short="i")]
+  integer: bool,
 
   /// Prints the state vector of the simulation.
   #[structopt(long)]
@@ -66,22 +85,42 @@ fn print_result(result: &Run, options: &Options) -> fmt::Result {
   let mut buffer = String::new();
 
   if let Some(_) = options.shots {
+    if options.verbose > 0 {
+      writeln!(&mut buffer, "Memory histogram:")?;
+    }
     let histogram = result.histogram.as_ref().expect("there is some histogram");
-    print_histogram(&mut buffer, histogram).expect("can print");
+    print_histogram(&mut buffer, histogram, options).expect("can print");
+  }
+  else {
+    if options.verbose > 0 {
+      writeln!(&mut buffer, "Memory:")?;
+    }
+    print_memory(&mut buffer, &result.memory, options).expect("can print");
+  }
+  if options.verbose > 0 {
+    writeln!(&mut buffer, "")?;
   }
 
-  print_memory(&mut buffer, &result.memory).expect("can print");
-
-  if options.statevector {
-    print_statevector(&mut buffer, &result.statevector).expect("can print");
+  if options.statevector || options.probabilities {
+    let statevector = if options.statevector { Some(&result.statevector) } else { None };
+    let probabilities = if options.probabilities { Some(&result.probabilities) } else { None };
+    if options.verbose > 0 {
+      writeln!(&mut buffer, "Simulation state:")?;
+    }
+    print_state(&mut buffer, statevector, probabilities).expect("can print");
   }
-
-  if options.probabilities {
-    print_probabilities(&mut buffer, &result.probabilities).expect("can print");
+  if options.verbose > 0 {
+    writeln!(&mut buffer, "")?;
   }
 
   if options.times {
+    if options.verbose > 0 {
+      writeln!(&mut buffer, "Times:")?;
+    }
     print_times(&mut buffer, &result.times).expect("can print");
+  }
+  if options.verbose > 0 {
+    writeln!(&mut buffer, "")?;
   }
 
   Ok(match &options.output {
@@ -90,39 +129,171 @@ fn print_result(result: &Run, options: &Options) -> fmt::Result {
   })
 }
 
-fn print_histogram(buffer: &mut String, histogram: &Histogram) -> fmt::Result {
-  for (key, values) in histogram {
-    let entries_str: Vec<String> = values.iter().map(|(x, y)| format!("({}: {})", x, y)).collect();
-    writeln!(buffer, "{} => [{}]", &key, entries_str.join(", "))?;
-  }
-  Ok(())
+fn print_memory(buffer: &mut String, memory: &HashMap<String, u64>, options: &Options) -> fmt::Result {
+  let histogram = HashMap::from_iter(
+    memory.iter().map(|(key, value)| (key.clone(), vec![(*value, 1)]))
+  );
+  print_universal_table(buffer, &histogram, options, true)
 }
 
-fn print_memory(buffer: &mut String, memory: &HashMap<String, u64>) -> fmt::Result {
-  for (key, value) in memory {
-    writeln!(buffer, "{} => {}", key, value)?;
-  }
-  Ok(())
+fn print_histogram(buffer: &mut String, histogram: &Histogram, options: &Options) -> fmt::Result {
+  print_universal_table(buffer, histogram, options, false)
 }
 
-fn print_probabilities(buffer: &mut String, probabilities: &Vec<f64>) -> fmt::Result {
-  let width = format!("{}", probabilities.len() - 1).len();
-  for (idx, chance) in probabilities.iter().enumerate() {
-    writeln!(buffer, "{1: >0$}: {2:.6}%", width, idx, *chance * 100.0)?;
+fn print_universal_table(buffer: &mut String, histogram: &Histogram, options: &Options, omit_count: bool) -> fmt::Result {
+  let mut table = Table::new();
+  table.set_format(*format::consts::FORMAT_NO_LINESEP_WITH_TITLE);
+
+  let binary = options.binary;
+  let hexadecimal = options.hexadecimal;
+  let integer = if binary || hexadecimal { options.integer } else { true };
+
+  match (integer, hexadecimal, binary) {
+    (false, false, true) => {
+      let mut titles = row![c -> "Name", c -> "Bin value"];
+      if !omit_count { titles.add_cell(cell!(c -> "Count")); }
+      table.set_titles(titles);
+
+      for (key, hist) in histogram {
+        for (idx, (value, count)) in hist.iter().enumerate() {
+          table.add_row(row![
+            r -> if idx == 0 { key } else { "" },
+            r -> format!("0b{:b}", value)
+          ]);
+          let last_row = table.len() - 1;
+          if !omit_count { table[last_row].add_cell(cell!(r -> count)); }
+        }
+      }
+    },
+    (false, true, false) => {
+      table.set_titles(row![c -> "Name", c -> "Hex value", c -> "Count"]);
+      for (key, hist) in histogram {
+        for (idx, (value, count)) in hist.iter().enumerate() {
+          table.add_row(row![
+            r -> if idx == 0 { key } else { "" },
+            r -> format!("0x{:x}", value),
+            r -> count
+          ]);
+        }
+      }
+    },
+    (false, true, true) => {
+      table.set_titles(row![c -> "Name", c -> "Hex value", c -> "Bin value", c -> "Count"]);
+      for (key, hist) in histogram {
+        for (idx, (value, count)) in hist.iter().enumerate() {
+          table.add_row(row![
+            r -> if idx == 0 { key } else { "" },
+            r -> format!("0x{:x}", value),
+            r -> format!("0b{:b}", value),
+            r -> count
+          ]);
+        }
+      }
+    },
+    (true, false, false) => {
+      table.set_titles(row![c -> "Name", c -> "Int value", c -> "Count"]);
+      for (key, hist) in histogram {
+        for (idx, (value, count)) in hist.iter().enumerate() {
+          table.add_row(row![
+            r -> if idx == 0 { key } else { "" },
+            r -> value,
+            r -> count
+          ]);
+        }
+      }
+    },
+    (true, false, true) => {
+      table.set_titles(row![c -> "Name", c -> "Int value", c -> "Bin value", c -> "Count"]);
+      for (key, hist) in histogram {
+        for (idx, (value, count)) in hist.iter().enumerate() {
+          table.add_row(row![
+            r -> if idx == 0 { key } else { "" },
+            r -> value,
+            r -> format!("0b{:b}", value),
+            r -> count
+          ]);
+        }
+      }
+    },
+    (true, true, false) => {
+      table.set_titles(row![c -> "Name", c -> "Int value", c -> "Hex value", c -> "Count"]);
+      for (key, hist) in histogram {
+        for (idx, (value, count)) in hist.iter().enumerate() {
+          table.add_row(row![
+            r -> if idx == 0 { key } else { "" },
+            r -> value,
+            r -> format!("0x{:x}", value),
+            r -> count
+          ]);
+        }
+      }
+    },
+    (true, true, true) => {
+      table.set_titles(row![c -> "Name", c -> "Int value", c -> "Hex value", c -> "Bin value", c -> "Count"]);
+      for (key, hist) in histogram {
+        for (idx, (value, count)) in hist.iter().enumerate() {
+          table.add_row(row![
+            r -> if idx == 0 { key } else { "" },
+            r -> value,
+            r -> format!("0x{:x}", value),
+            r -> format!("0b{:b}", value),
+            r -> count
+          ]);
+        }
+      }
+    },
+    _ => unreachable!("all headers possibilities should be covered.")
   }
-  Ok(())
+
+  write!(buffer, "{}", table)
 }
 
-fn print_statevector(buffer: &mut String, statevector: &StateVector) -> fmt::Result {
-  let width = format!("{}", statevector.len() - 1).len();
-  for (idx, amplitude) in statevector.bases.iter().enumerate() {
-    writeln!(buffer, "{1: >0$}: {2:.6}", width, idx, amplitude)?;
+
+fn print_state(buffer: &mut String, statevector: Option<&StateVector>, probabilities: Option<&Vec<f64>>) -> fmt:: Result {
+  let mut table = Table::new();
+  table.set_format(*format::consts::FORMAT_NO_LINESEP_WITH_TITLE);
+
+  match (statevector, probabilities) {
+    (None, None) => panic!("at least one of probabibilities or statevector should be provided"),
+    (None, Some(probabilities)) => {
+      table.set_titles(row!["Base", "Probability"]);
+      for (idx, chance) in probabilities.iter().enumerate() {
+        table.add_row(row![idx, format!("{:.6}", chance)]);
+      }
+    }
+    (Some(statevector), None) => {
+      table.set_titles(row!["Base", "Real", "Imaginary"]);
+      for (idx, chance) in statevector.bases.iter().enumerate() {
+        table.add_row(row![
+          idx,
+          format!("{:.6}", chance.re),
+          format!("{:.6}", chance.im)
+        ]);
+      }
+    }
+    (Some(statevector), Some(probabilities)) => {
+      table.set_titles(row!["Base", "Real", "Imaginary", "Probability"]);
+      for (idx, chance) in statevector.bases.iter().enumerate() {
+        table.add_row(row![
+          idx,
+          format!("{:.6}", chance.re),
+          format!("{:.6}", chance.im),
+          format!("{:.6}", probabilities[idx])
+        ]);
+      }
+    }
   }
-  Ok(())
+
+  write!(buffer, "{}", table)
 }
 
 fn print_times(buffer: &mut String, times: &RunTimes) -> fmt::Result {
-  writeln!(buffer, "parsing_time: {}", times.parsing_time)?;
-  writeln!(buffer, "simulation_time: {}", times.simulation_time)?;
-  Ok(())
+  let mut table = Table::new();
+  table.set_format(*format::consts::FORMAT_NO_LINESEP_WITH_TITLE);
+
+  table.set_titles(row!["Name", "Duration (ms)"]);
+  table.add_row(row!["parsing", times.parsing_time]);
+  table.add_row(row!["simulation", times.simulation_time]);
+
+  write!(buffer, "{}", table)
 }
