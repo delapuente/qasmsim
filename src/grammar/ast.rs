e35b125bbbc380ec6298e87b1a5235f7312e71f4
Salvador de la Puente González <salva@unoyunodiez.com>
2020-04-27 22:54:36 +0200

    Finish ast documentation

--- src/grammar/ast.rs
+++ src/grammar/ast.rs
@@ -29,6 +29,18 @@ use crate::grammar::lexer::Location;
 /// Can be built programmatically with:
 ///
 /// ```
+/// use qasmsim::grammar::ast::{
+///     OpenQasmProgram,
+///     Span,
+///     Statement,
+///     QuantumOperation,
+///     UnitaryOperation,
+///     Opcode,
+///     Expression,
+///     Argument
+/// };
+/// use qasmsim::grammar::lexer::Location;
+///
 /// let program = OpenQasmProgram {
 ///     version: "2.0".to_string(),
 ///     program: vec![
@@ -52,7 +64,7 @@ use crate::grammar::lexer::Location;
 ///                                 Expression::Op(
 ///                                     Opcode::Div,
 ///                                     Box::new(Expression::Pi),
-///                                     Box::new(Int(2))
+///                                     Box::new(Expression::Int(2))
 ///                                 ),
 ///                                 Expression::Int(0),
 ///                                 Expression::Pi
@@ -85,7 +97,7 @@ pub struct OpenQasmProgram {
 /// The AST corresponding to the following library:
 ///
 /// ```qasm
-/// gate h {
+/// gate h q {
 ///     U(pi/2, 0, pi) q[0];
 /// }
 /// ```
@@ -93,32 +105,43 @@ pub struct OpenQasmProgram {
 /// Can be built programmatically with:
 ///
 /// ```
+/// use qasmsim::grammar::ast::{
+///     OpenQasmLibrary,
+///     Statement,
+///     GateOperation,
+///     UnitaryOperation,
+///     Opcode,
+///     Expression,
+///     Argument
+/// };
+/// use qasmsim::grammar::lexer::Location;
+///
 /// let library = OpenQasmLibrary {
 ///     definitions: vec![
-///         Span {
-///             boundaries: (Location(0), Location(35)),
-///             node: Box::new(
-///                 Statement::QuantumOperation(
-///                     QuantumOperation::Unitary(
-///                         UnitaryOperation(
-///                             "U".to_string(),
-///                             vec![
-///                                 Expression::Op(
-///                                     Opcode::Div,
-///                                     Box::new(Expression::Pi),
-///                                     Box::new(Int(2))
-///                                 ),
-///                                 Expression::Int(0),
-///                                 Expression::Pi
-///                             ],
-///                             vec![
-///                                 Argument::Item("q".to_string(), 0)
-///                             ]
-///                         )
+///         Statement::GateDecl(
+///             "h".to_string(),
+///             vec![],
+///             vec!["q".to_string()],
+///             vec![
+///                 GateOperation::Unitary(
+///                     UnitaryOperation(
+///                        "U".to_string(),
+///                         vec![
+///                             Expression::Op(
+///                                 Opcode::Div,
+///                                 Box::new(Expression::Pi),
+///                                 Box::new(Expression::Int(2))
+///                             ),
+///                             Expression::Int(0),
+///                             Expression::Pi
+///                         ],
+///                         vec![
+///                             Argument::Item("q".to_string(), 0)
+///                         ]
 ///                     )
 ///                 )
-///             )
-///         }
+///             ]
+///         )
 ///     ]
 /// };
 #[derive(Debug, Clone, PartialEq)]
@@ -141,17 +164,22 @@ pub struct OpenQasmLibrary {
 /// The AST corresponding to the `barrier` of the following program:
 ///
 /// ```qasm
-/// barrir q;
+/// barrier q;
 /// ```
 ///
 /// Corresponds to:
 ///
 /// ```
+/// use qasmsim::grammar::ast::{BarrierPragma, Argument};
+///
 /// let barrier = BarrierPragma(vec![Argument::Id("q".to_string())]);
 /// ```
 ///
 /// A `BarrierPragma` cannot compound a valid [`OpenQasmProgram`]. It needs
 /// to be enclosed in a [`Statement`].
+///
+/// [`OpenQasmProgram`]: ./struct.OpenQasmProgram.html
+/// [`Statement`]: ./enum.Statement.html
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct BarrierPragma(pub Vec<Argument>);
 
@@ -172,12 +200,18 @@ pub struct BarrierPragma(pub Vec<Argument>);
 /// Is represented, as statement, like:
 ///
 /// ```
+/// use qasmsim::grammar::ast::{Statement, BarrierPragma, Argument};
+///
 /// let barrier_stmt = Statement::Barrier(
 ///     BarrierPragma(vec![Argument::Id("q".to_string())])
 /// );
 /// ```
 ///
-/// This statement can be aggregated to form a valid [`OpenQasmProgram`].
+/// Enclose a statement inside a [span] and aggregate them in a list to form
+/// a valid [`OpenQasmProgram`].
+///
+/// [span]: ./enum.Span.html
+/// [`OpenQasmProgram`]: ./struct.OpenQasmProgram.html
 #[non_exhaustive]
 #[derive(Debug, Clone, PartialEq)]
 pub enum Statement {
@@ -201,63 +235,197 @@ pub enum Statement {
     /// A wrapper for making a quantum operation to execute just if certain
     /// equality condition holds. The wrapper takes the left-side of the
     /// comparison, the right side, and the operation to perform.
-    Conditional(Argument, u64, QuantumOperation),
+    Conditional(Argument, u64, QuantumOperation)
 }
 
+/// Relates a node with the fragment of source code where the node appears.
+///
+/// # Examples
+///
+/// Consider the following program:
+///
+/// ```qasm
+/// OPENQASM 2.0;
+/// qdef q[10];
+/// ```
+///
+/// The span for the second line statement is as follows:
+///
+/// ```
+/// use qasmsim::grammar::ast::{Span, Statement};
+/// use qasmsim::grammar::lexer::Location;
+///
+/// let barrier_span = Span {
+///     boundaries: (Location(14), Location(25)),
+///     node: Box::new(
+///         Statement::QRegDecl(
+///             "q".to_string(),
+///             1
+///         )
+///     )
+/// };
+/// ```
+///
+/// Boundaries run from characters 14 to 25 corresponding to the starting-0
+/// character index of the source code.
+///
+/// Right, now, only statements are tied to spans making impossible to
+/// accurately localize inner AST nodes.
 #[derive(Debug, Clone, PartialEq)]
 pub struct Span<S> {
+    /// Pair of source locations where the AST node can be found.
     pub boundaries: (Location, Location),
+    /// Boxed AST node.
     pub node: Box<S>,
 }
 
+/// Any of the statements that can appear inside a gate definition.
+///
+/// # Examples
+///
+/// See [`OpenQasmLibrary`] for a complete example.
+///
+/// A gate in OPENQASM is always reversible so gates can only be compound of
+/// barriers (which are no-op actually) and other gate invocations.
+///
+/// [`OpenQasmLibrary`]: ./struct.OpenQasmLibrary.html
+#[non_exhaustive]
 #[derive(Debug, Clone, PartialEq)]
 pub enum GateOperation {
+    /// A gate invocation.
     Unitary(UnitaryOperation),
+    /// A barrier pragma.
     Barrier(BarrierPragma),
 }
 
+/// Any of the operations that actuates over quantum registers.
+///
+/// # Examples
+///
+/// See [`OpenQasmProgram`] for a complete examples.
+///
+/// [`OpenQasmProgram`]: ./struct.OpenQasmProgram.html
+#[non_exhaustive]
 #[derive(Debug, Clone, PartialEq)]
 pub enum QuantumOperation {
+    /// A gate invocation.
     Unitary(UnitaryOperation),
+    /// A measurement on a quantum register to a classical register.
     Measure(Argument, Argument),
+    /// A reset operation on a quantum register.
     Reset(Argument),
 }
 
+/// A gate "invocation".
+///
+/// The name comes after the fact that all quantum gates are [unitary]
+/// operators. Calling a gate consists on applying it on some quantum
+/// registers.
+///
+/// # Examples
+///
+/// See [`OpenQasmProgram`] for a complete example.
+///
+/// [`OpenQasmProgram`]: ./struct.OpenQasmProgram.html
+/// [unitary]: https://en.wikipedia.org/wiki/Unitary_operator
 #[derive(Debug, Clone, PartialEq)]
 pub struct UnitaryOperation(pub String, pub Vec<Expression>, pub Vec<Argument>);
 
+/// Any of the operators that can appear in an expression.
+///
+/// # Examples
+///
+/// Notice the different `Expression` instances in the [`OpenQasmLibrary`]
+/// example.
+///
+/// [`OpenQasmLibrary`]: ./struct.OpenQasmLibrary.html
+#[non_exhaustive]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum Opcode {
+    /// Code for the addition operator `+`.
     Add,
+    /// Code for the substraction operator `-`.
     Sub,
+    /// Code for the multiplication operator `*`.
     Mul,
+    /// Code for the division operator `/`.
     Div,
+    /// Code for the power operator `^`.
     Pow,
 }
 
+/// Any of the functions that can appear in an expression.
+#[non_exhaustive]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum Funccode {
+    /// Function sinus `sin`.
     Sin,
+    /// Function cosinus `cos`.
     Cos,
+    /// Function tangent `tan`.
     Tan,
+    /// Function exponential `exp`.
     Exp,
+    /// Function natural logarithm `ln`.
     Ln,
+    /// Function square root `sqrt`.
     Sqrt,
 }
 
+/// Any of the subexpressions that can appear inside a expression.
+///
+/// # Examples
+///
+/// See [`OpenQasmLibrary`] for an example.
+///
+/// [`OpenQasmLibrary`]: ./struct.OpenQasmLibrary.html
+#[non_exhaustive]
 #[derive(Debug, Clone, PartialEq)]
 pub enum Expression {
+    /// The pi constant `pi`.
     Pi,
+    /// A valid OPENQASM identifier.
     Id(String),
+    /// A real number.
     Real(f64),
+    /// An integer number.
     Int(u64),
+    /// A binary operation.
     Op(Opcode, Box<Expression>, Box<Expression>),
+    /// A call to a function.
     Function(Funccode, Box<Expression>),
+    /// A negation of an expression.
     Minus(Box<Expression>),
 }
 
+/// A reference to a register or register component.
+///
+/// # Examples
+///
+/// Look at these barrier statements:
+///
+/// ```qasm
+/// barrier q;
+/// barrier q[0];
+/// ```
+///
+/// They differ on the quantum register argument and can be built with:
+///
+/// ```
+/// use qasmsim::grammar::ast::{BarrierPragma, Argument};
+///
+/// let on_the_whole_register = BarrierPragma(
+///     vec![Argument::Id("q".to_string())]
+/// );
+/// let on_the_first_qubit = BarrierPragma(
+///     vec![Argument::Item("q".to_string(), 0)]
+/// );
+/// ```
+#[non_exhaustive]
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum Argument {
+    /// An entire register like `q`.
     Id(String),
+    /// One of the bits/qubits of a register `q[0]`.
     Item(String, usize),
 }
