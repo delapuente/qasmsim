2cf797040922b147c665d21e2647183c3eb31e02
Salvador de la Puente González <salva@unoyunodiez.com>
2020-04-27 10:14:59 +0200

    WIP: Comment the public API of qasmsim

--- src/grammar/ast.rs
+++ src/grammar/ast.rs
@@ -1,10 +1,22 @@
-//! Contain all the AST data structures conforming a valid OPENQASM program.
+//! Contain the data structures for creating OPENQASM ASTs. The module is
+//! **unstable**.
+//!
+//! # Notes
+//!
+//! Although OPENQASM 2.0 is stable enough, this module is not. Providing better
+//! errors would require an extensive use of the `Span` structure beyond
+//! statements, and adding new features to the language would require the
+//! modification os certain layouts.
+//!
+//! The module would remain in the public API for enabling the users to perform
+//! code manipulations at the abstract level.
+
 use crate::grammar::lexer::Location;
 
 /// Represent a OPENQASM program. A valid program contains a version string
 /// and a list of instructions.
 ///
-/// # Examples:
+/// # Examples
 ///
 /// The AST corresponding to the following program:
 ///
@@ -68,7 +80,7 @@ pub struct OpenQasmProgram {
 /// Represent a OPENQASM library. OPENQASM libraries can contain gate
 /// declarations only.
 ///
-/// # Examples:
+/// # Examples
 ///
 /// The AST corresponding to the following library:
 ///
@@ -119,11 +131,12 @@ pub struct OpenQasmLibrary {
 
 // TODO: This should not be part of the grammar. It is a directive for
 // the optimizer or compiler.
+
 /// A pragma for a potential gate optimizer to prevent the combination of the
 /// gates at both sides of the barrier. The barrier takes a list of registers
 /// or qubits arguments.
 ///
-/// # Examples:
+/// # Examples
 ///
 /// The AST corresponding to the `barrier` of the following program:
 ///
@@ -134,21 +147,60 @@ pub struct OpenQasmLibrary {
 /// Corresponds to:
 ///
 /// ```
-/// let barrier = BarrierPragmae(vec![Argument::Id("q".to_string())]);
+/// let barrier = BarrierPragma(vec![Argument::Id("q".to_string())]);
 /// ```
+///
+/// A `BarrierPragma` cannot compound a valid [`OpenQasmProgram`]. It needs
+/// to be enclosed in a [`Statement`].
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct BarrierPragma(pub Vec<Argument>);
 
+/// Each of the statements you can find in a OPENQASM program.
 ///
+/// OPENQASM programs are made exclusively of list of statements. The statements
+/// can be wrappers for more complex structures and take parameters representing
+/// these structures.
+///
+/// # Examples
+///
+/// The following OPENQASM code:
+///
+/// ```qasm
+/// barrier q;
+/// ```
+///
+/// Is represented, as statement, like:
+///
+/// ```
+/// let barrier_stmt = Statement::Barrier(
+///     BarrierPragma(vec![Argument::Id("q".to_string())])
+/// );
+/// ```
+///
+/// This statement can be aggregated to form a valid [`OpenQasmProgram`].
+#[non_exhaustive]
 #[derive(Debug, Clone, PartialEq)]
 pub enum Statement {
+    /// Quantum register declaration with name and size.
     QRegDecl(String, usize),
+    /// Classical register declaration with name and size.
     CRegDecl(String, usize),
+    /// Quantum gate declaration with name, list of formal real parameters,
+    /// list of formal quantum registers, and a list of [`GateOperation`]
+    /// representing the body of the gate.
     GateDecl(String, Vec<String>, Vec<String>, Vec<GateOperation>),
+    /// Include statement for linking with gate libraries.
     Include(String),
+    /// A wrapper for the barrier pragma.
     Barrier(BarrierPragma),
+    /// Opaque gate declaration with name and formal lists of real parameters
+    /// and quantum registers. Opaque declarations have no body.
     OpaqueGateDecl(String, Vec<String>, Vec<String>),
+    /// A wrapper for a quantum operation.
     QuantumOperation(QuantumOperation),
+    /// A wrapper for making a quantum operation to execute just if certain
+    /// equality condition holds. The wrapper takes the left-side of the
+    /// comparison, the right side, and the operation to perform.
     Conditional(Argument, u64, QuantumOperation),
 }
 
