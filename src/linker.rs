e428a68b4a5194b00b40e2c8a22d260a208c4a29
Salvador de la Puente González <salva@unoyunodiez.com>
2020-05-04 12:37:41 +0200

    Document the linker module

--- src/linker.rs
+++ src/linker.rs
@@ -4,11 +4,76 @@ use crate::grammar::ast;
 use crate::grammar::open_qasm2;
 use crate::grammar::lexer::{Lexer, Location};
 
+/// Represent a filure during linkage.
+///
+/// # Examples
+///
+/// The following typo in OPENQASM:
+///
+/// ```qasm
+/// OPENQASM 2.0;
+/// include "qlib.inc"  // instead of "qelib1.inc"
+/// ```
+///
+/// Would cause the following error:
+///
+/// ```qasm
+/// use qasmsim::grammar::lexer::Location;
+/// use qasmsim::linker::LinkerError;
+///
+/// LinkerError::LibraryNotFound {
+///     location: Location(14),
+///     libpath: "qlib.inc".to_string()
+/// }
+/// ```
+#[non_exhaustive]
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum LinkerError {
-    LibraryNotFound { location: Location, libpath: String },
+    /// The `include` directive at `location` failed while importing `libpath`.
+    LibraryNotFound {
+        /// Location of the `include` directive.
+        location: Location,
+        /// Library path passed to the `include` directive.
+        libpath: String
+    },
 }
 
+
+/// Allow for combining multiple AST, spread in several sources, into one.
+///
+/// Current implementation is incomplete as it only allows for using embedded
+/// resources without looking for sources in the file system.
+///
+/// # Examples
+///
+/// You can provide a custom embedded library `ghz.inc` with:
+///
+/// ```
+/// use std::collections::HashMap;
+/// use std::iter::FromIterator;
+/// use qasmsim::linker::Linker;
+///
+/// let linker = Linker::with_embedded(HashMap::from_iter(vec![
+///     (
+///         "ghz.inc".to_string(),
+///         r#"gate ghz q {
+///             U(pi/2, 0, pi) q[0];
+///             CX(q[0], q[1]);
+///             CX(q[0], q[2]);
+///         }"#.to_string()
+///     )
+/// ]));
+/// ```
+///
+/// The linker can be passed to [`compile_with_linker`] to enable using the
+/// `ghz.inc` library in your programs like:
+///
+/// ```qasm
+/// OPENQASM 2.0;
+/// include "ghz.inc";
+/// qdef q[3];
+/// qhz q;
+/// ```
 #[derive(Debug, Clone, Default)]
 pub struct Linker {
     embedded: HashMap<String, String>,
@@ -17,16 +82,20 @@ pub struct Linker {
 type Result<T> = std::result::Result<T, LinkerError>;
 
 impl Linker {
+    /// Create a new linker with no embedded sources.
     pub fn new() -> Self {
         Linker {
             embedded: Default::default(),
         }
     }
 
+    /// Create a new linker with a hashmap relating paths with embedded sources.
     pub fn with_embedded(embedded: HashMap<String, String>) -> Self {
         Linker { embedded }
     }
 
+    /// Look into `tree` for `include` statements, parse the referred libraries,
+    /// and integrate their ASTs into `tree`, effectively modifying `tree`.
     pub fn link(&self, mut tree: ast::OpenQasmProgram) -> Result<ast::OpenQasmProgram> {
         let mut to_embed = vec![];
         for (index, span) in tree.program.iter().enumerate() {
@@ -34,13 +103,13 @@ impl Linker {
                 let source = self
                     .sources(&libpath)
                     .map_err(|_| LinkerError::LibraryNotFound {
-                        location: span.boundaries.0.clone(),
+                        location: span.boundaries.0,
                         libpath: libpath.into(),
                     })?;
                 let lexer = Lexer::new(&source);
                 let parser = open_qasm2::OpenQasmLibraryParser::new();
                 let library_tree = parser.parse(lexer).unwrap();
-                to_embed.push((index, span.boundaries.clone(), library_tree.definitions));
+                to_embed.push((index, span.boundaries, library_tree.definitions));
             }
         }
         to_embed.reverse();
@@ -48,7 +117,7 @@ impl Linker {
             let mut inner_spans = vec![];
             for one_statement in statements {
                 inner_spans.push(ast::Span {
-                    boundaries: boundaries.clone(),
+                    boundaries,
                     node: Box::new(one_statement),
                 })
             }
