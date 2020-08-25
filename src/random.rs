67cd61d7cf2de6a7eee361bca4fd7e7f6c0ae229
Salvador de la Puente González <salva@unoyunodiez.com>
2020-08-26 01:06:44 +0200

    Add explanation of the `random` crate

--- src/random.rs
+++ src/random.rs
@@ -1,3 +1,12 @@
+//! This module exists to force using JavaScript `Math.random()` in replacement
+//! of Rust `rand::random()` when targeting wasm32-unknown-unknown
+//! architecture.
+//!
+//! For some reason, not using it, although [after enabling WASM features]
+//! causes the module `cryto` not to be found.
+//!
+//! [after enabling WASM features]: https://rust-random.github.io/book/crates.html?highlight=wasm#wasm-support
+
 #[cfg(not(target_arch = "wasm32"))]
 pub(crate) fn random() -> f64 {
     rand::random()
